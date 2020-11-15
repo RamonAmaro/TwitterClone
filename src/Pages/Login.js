69677c780be9f7e92adc9c1a06d28ded21f1a66f
Ramon Amaro <ramonamaro.dev@gmail.com>
2020-11-15 08:45:52 -0300

    New Features

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 import './Login.css'
 import Logo from '../assets/twitterLogo.png'
 
-// import { Container } from './styles';
-
 function Login() {
+    const [inputValue, setInputValue] = useState('')
+    const history = useHistory();
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!inputValue.length) return;
+        localStorage.setItem('username', inputValue)
+        history.push('/timeline')
+    }
     return (
         <div className="login-wrapper">
             <img src={Logo} alt=".." style={{ width: '40px', height: '40px' }} />
-            <form>
-                <input placeholder="nome de usuario" />
-                <button type="submit"> Entrar  </button>
+            <form onSubmit={handleSubmit}>
+                <input name="user" value={inputValue}
+                    placeholder="nome de usuario"
+                    onChange={handleInputChange} />
+                <button type="submit"> Entrar </button>
             </form>
         </div>
     )
--- src/Pages/Timeline.js
+++ src/Pages/Timeline.js
@@ -1,9 +1,39 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useState } from 'react';
+import './Timeline.css'
+import TweeterLogo from '../assets/twitterLogo.png'
 
 function Timeline() {
-    return <h1> Timeline</h1>
+    const [textArea, setTextArea] = useState('')
+
+    const handleInputChange = (e) => {
+        setTextArea(e.target.value)
+    }
+    const handleNewTweet = (e) => {
+        if (e.keyCode !== 13) return;
+        e.preventDefault();
+
+        const content = textArea;
+        const author = localStorage.getItem('username')
+
+        console.log(content, author)
+    }
+
+    return (
+        <div className="timeline-wrapper">
+            <img src={TweeterLogo} alt="..." style={{ width: '24px', height: '24px' }} />
+
+            <form>
+                <input
+                    type="text"
+                    value={textArea}
+                    onChange={handleInputChange}
+                    onKeyDown={handleNewTweet}
+                    placeholder="oque está acontecendo ?"
+                />
+            </form>
+
+        </div>
+    )
 }
 
 export default Timeline;
